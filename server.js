3817ebc8a6e03a8b84949ed8c03a9ad2bd5c1e9a
zacktmiller92 <zacktmiller92@gmail.com>
2021-09-19 16:52:09 -0400

    feature/html-views-and-routes

--- server.js
+++ server.js
@@ -3,8 +3,10 @@ const routes = require('./controllers');
 const sequelize = require('./config/connection');
 const path = require('path');
 
+const helpers = require('./utils/helpers');
+
 const exphbs = require('express-handlebars');
-const hbs = exphbs.create();
+const hbs = exphbs.create({ helpers });
 
 
 
