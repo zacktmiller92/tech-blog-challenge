a18482c4fffbb1b6f6e734e1024facfe0820405f
zacktmiller92 <zacktmiller92@gmail.com>
2021-09-19 16:36:53 -0400

    added model relationships in index.js

--- models/index.js
+++ models/index.js
@@ -1,4 +1,31 @@
-const User = require('./user');
+// import all models
+const Post = require('./Post');
+const User = require('./User');
+const Comment = require('./Comment');
 
+// create associations
+User.hasMany(Post, {
+  foreignKey: 'user_id'
+});
 
-module.exports = { User };
+Post.belongsTo(User, {
+  foreignKey: 'user_id'
+});
+
+Comment.belongsTo(User, {
+  foreignKey: 'user_id'
+});
+
+Comment.belongsTo(Post, {
+  foreignKey: 'post_id'
+});
+
+User.hasMany(Comment, {
+  foreignKey: 'user_id'
+});
+
+Post.hasMany(Comment, {
+  foreignKey: 'post_id'
+});
+
+module.exports = { User, Post, Comment };
