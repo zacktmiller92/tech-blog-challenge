436897a24a489592fc91b39f3b21fbd00efc157c
zacktmiller92 <zacktmiller92@gmail.com>
2021-09-20 20:56:44 -0400

    fixed capitlizations

--- models/index.js
+++ models/index.js
@@ -1,9 +1,7 @@
-// import all models
 const Post = require('./Post');
 const User = require('./User');
 const Comment = require('./Comment');
 
-// create associations
 User.hasMany(Post, {
   foreignKey: 'user_id'
 });
